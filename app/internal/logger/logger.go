b868181064b09ad8304f38e131c5b98cd75f88f5
B0fk <serega18102002@gmail.com>
2025-07-30 00:24:35 +0700

    fix docker

--- app/internal/logger/logger.go
+++ app/internal/logger/logger.go
@@ -1,9 +1,12 @@
 package logger
 
 import (
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"io"
 	"os"
+	"path"
+	"runtime"
 )
 
 type writerHook struct {
@@ -44,6 +47,10 @@ func Init() {
 	l := logrus.New()
 	l.SetReportCaller(true)
 	l.Formatter = &logrus.TextFormatter{
+		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
+			fileName := path.Base(frame.File)
+			return fmt.Sprintf("%s", frame.Function), fmt.Sprintf("%s:%d", fileName, frame.Line)
+		},
 		DisableColors: false,
 		FullTimestamp: true,
 		ForceColors:   true,
